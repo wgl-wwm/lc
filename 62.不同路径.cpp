63338f4cd2f676b6061edb81098c17fdd268446d
Lonvergence <1156997364@qq.com>
2022-08-10 11:50:49 +0800

    try

---
+++ 62.不同路径.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=cpp
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+
+    }
+};
+// @lc code=end
+
